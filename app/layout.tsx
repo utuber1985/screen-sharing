71a0967b398c62199e35acffbfb9090c8daf35d6
Hin <tonghohin77@gmail.com>
2024-11-30 01:19:46 -0500

    chore: remove Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
 import { Clarity } from "@/components/Clarity";
 import { Toaster } from "@/components/ui/toaster";
-import { Analytics } from "@vercel/analytics/react";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
     title: "Screen Share - Share Your Screen Instantly",
     description: "Share your screen instantly with anyone using a simple room code. No downloads or sign-ups required.",
     keywords: "screen sharing, webrtc, online screen share, browser screen sharing, free screen sharing"
-};
+} satisfies Metadata;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
             <body className={inter.className}>
                 {children}
-                <Analytics />
                 <Clarity />
                 <Toaster />
             </body>
