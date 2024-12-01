a944fe1613e393c1d6741918216cc5ecf9a3a138
Hin <tonghohin77@gmail.com>
2024-12-01 11:56:34 -0500

    feat: move Clarity project id to env

--- components/Clarity.tsx
+++ components/Clarity.tsx
@@ -1,6 +1,10 @@
 import Script from "next/script";
 
 export function Clarity() {
+    const clarityProjectId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID;
+
+    if (!clarityProjectId) return null;
+
     return (
         <Script id="microsoft-clarity" type="text/javascript">
             {`
@@ -8,7 +12,7 @@ export function Clarity() {
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "p69zy6fidj");
+                })(window, document, "clarity", "script", "${clarityProjectId}");
             `}
         </Script>
     );
